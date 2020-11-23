e3fb0b9dc32edae9dfb7d164dfd7e0dc1b71c32b
pnguye11 <pnguye11@gmail.com>
2020-11-22 16:26:24 -0800

    lib init and DB up

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const { Config } = require("node-json-db/dist/lib/JsonDBConfig");
+const speakeasy = require("speakeasy");
+const uudi = require("uuid");
+const { JsonDB } = require("node-json-db");
 const app = express();
+const db = new JsonDB(new Config("myDatabase", true, false, "/"));
 
 app.get("/api", (req, res) => res.json({ message: "welcome to the 2fa" }));
 
