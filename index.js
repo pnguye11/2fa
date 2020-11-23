ede876eb0b84b9c4e144f28c5a277ec3704e3696
pnguye11 <pnguye11@gmail.com>
2020-11-22 16:17:36 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/api", (req, res) => res.json({ message: "welcome to the 2fa" }));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server running on port${PORT}`));
